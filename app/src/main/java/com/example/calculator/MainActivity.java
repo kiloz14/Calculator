3d94a5faab84009535ba603dd559da477b2c404c
Kwamina Thompson <kwaminathompson14@gmail.com>
2020-05-28 11:19:56 -0400

    Created an on view User click listerner

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -29,9 +30,68 @@ public class MainActivity extends AppCompatActivity {
         final Button divisionButton = findViewById(R.id.division);
         final Button moduloButton = findViewById(R.id.modulo);
         final Button equalButton = findViewById(R.id.equal);
-        final Button multiplyButton = findViewById(R.id.num1);
+        final Button multiplyButton = findViewById(R.id.multiplication);
 
 
+    final View.OnClickListener calculatorclickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            final int id = view.getId();
+            switch (id)
+            {
+                case R.id.num0:
+                    break;
+                case R.id.num1:
+                    break;
+                case R.id.num2:
+                    break;
+                case R.id.num3:
+                    break;
+                case R.id.num4:
+                    break;
+                case R.id.num5:
+                    break;
+                case R.id.num6:
+                    break;
+                case R.id.num7:
+                    break;
+                case R.id.num8:
+                    break;
+                case R.id.num9:
+                    break;
+                case R.id.addition:
+                    break;
+                case R.id.subtraction:
+                    break;
+                case R.id.division:
+                    break;
+                case R.id.multiplication:
+                    break;
+                case R.id.modulo:
+                    break;
+                case R.id.equal:
+                    break;
+            }
+
+        }
+    };
+
+        number0.setOnClickListener(calculatorclickListener);
+        number1.setOnClickListener(calculatorclickListener);
+        number2.setOnClickListener(calculatorclickListener);
+        number3.setOnClickListener(calculatorclickListener);
+        number4.setOnClickListener(calculatorclickListener);
+        number5.setOnClickListener(calculatorclickListener);
+        number6.setOnClickListener(calculatorclickListener);
+        number7.setOnClickListener(calculatorclickListener);
+        number8.setOnClickListener(calculatorclickListener);
+        number9.setOnClickListener(calculatorclickListener);
+        addButton.setOnClickListener(calculatorclickListener);
+        subButton.setOnClickListener(calculatorclickListener);
+        multiplyButton.setOnClickListener(calculatorclickListener);
+        moduloButton.setOnClickListener(calculatorclickListener);
+        divisionButton.setOnClickListener(calculatorclickListener);
+        equalButton.setOnClickListener(calculatorclickListener);
 
 
     }// End of Oncreate Bundle
